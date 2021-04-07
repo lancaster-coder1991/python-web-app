5b0a92679ae05d1437408ad78832668dd20c5074
George Scott <george.scott@bjss.com>
2021-04-07 12:40:22 +0100

    added image resizing and author images to home page

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import (
     User,
 )  # In the Profile model, we want to set up a 1-1 relationship with each user on the DB - see below for how this is implemented
+from PIL import Image
 
 
 class Profile(
@@ -16,6 +17,18 @@ class Profile(
 
     def __str__(self):
         return f"{self.user.username}'s Profile"
+
+    def save(
+        self,
+    ):  # All models have a default save method, but this can be overwritten as here
+        super.save()  # We still want to run the saving functionality from the default save function
+        img = Image.open(
+            self.image.path
+        )  # Using the Image class imported from Pillow, we can open and resize user uploaded images using its path
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
 
 
 # Create your models here.
